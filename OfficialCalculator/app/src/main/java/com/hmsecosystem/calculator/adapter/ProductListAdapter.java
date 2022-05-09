3e49567fcdaa45d5d92bc07046092d8126ed7d7b
Bryant Vu <40374800+bryantvu@users.noreply.github.com>
2022-05-09 15:51:12 -0700

    added futurewei header

--- OfficialCalculator/app/src/main/java/com/hmsecosystem/calculator/adapter/ProductListAdapter.java
+++ OfficialCalculator/app/src/main/java/com/hmsecosystem/calculator/adapter/ProductListAdapter.java
@@ -1,3 +1,16 @@
+/*
+      Copyright 2021. Futurewei Technologies Inc. All rights reserved.
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+        http:  www.apache.org/licenses/LICENSE-2.0
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+      limitations under the License.
+*/
+
 package com.hmsecosystem.calculator.adapter;
 
 import android.content.Context;
@@ -14,11 +27,6 @@ import com.huawei.hms.iap.entity.ProductInfo;
 
 import java.util.List;
 
-/**
- * Adapter for product list.
- *
- * @since 2019/12/9
- */
 public class ProductListAdapter extends BaseAdapter {
 
     // Context instance.
--- OfficialCalculator/app/src/main/java/com/hmsecosystem/calculator/common/ExceptionHandle.java
+++ OfficialCalculator/app/src/main/java/com/hmsecosystem/calculator/common/ExceptionHandle.java
@@ -1,3 +1,16 @@
+/*
+      Copyright 2021. Futurewei Technologies Inc. All rights reserved.
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+        http:  www.apache.org/licenses/LICENSE-2.0
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+      limitations under the License.
+*/
+
 package com.hmsecosystem.calculator.common;
 
 import static android.content.ContentValues.TAG;
@@ -9,11 +22,6 @@ import android.widget.Toast;
 import com.huawei.hms.iap.IapApiException;
 import com.huawei.hms.iap.entity.OrderStatusCode;
 
-/**
- *  Handles the exception returned from the iap api.
- *
- * @since 2019/12/9
- */
 public class ExceptionHandle {
     /**
      * The exception is solved.
